80aacce53c352957aff3964b22a474d89a05f921
Ryan Mahoney <ryan@virtuecenter.com>
2014-11-14 08:56:56 -0500

    progress

--- src/Comment/public/helpers/Comment.php
+++ src/Comment/public/helpers/Comment.php
@@ -1,5 +1,7 @@
 <?php
 return function ($arguments, $options) {
     $layout = \Opine\container()->layout;
+    $person = \Opine\container()->person;
+
     return $layout->make(['comment/index', 'Comment/index']);
 };
